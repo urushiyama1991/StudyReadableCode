971511f1cad18f1afad2704b067c413f93a0d9fb
urushiyama1991 <154032808+urushiyama1991@users.noreply.github.com>
2024-01-03 22:00:41 +0900

    Initial Commit

---
+++ StudyReadableCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StudyReadableCode
+//
+//  Created by sho urushiyama on 2024/01/03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
